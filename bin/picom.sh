fa3bfcb00e1a04b46edfa2f4c8eb3040d25e39ae
ivicente-pt <ilidio.vicente@gmail.com>
2026-03-30 02:27:40 +0100

    Muitas alterações. Integração com o aevh-lib.sh. Windoze agora é puro XFCE4.

--- bin/picom.sh
+++ bin/picom.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # picom.sh v1.1 2026-03-24
 
 xfconf-query -c xfwm4 -p /general/use_compositing -s false --create
-[[ "${PICOM:-0}" == "1" ]] && { sleep 3; picom -b --config /opt/aevh/etc/picom.conf; }
+[[ "${PICOM:-0}" == "1" ]] && { sleep 3; picom -b --config /opt/aevh/conf/picom/picom.conf; }
