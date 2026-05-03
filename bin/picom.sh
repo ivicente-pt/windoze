2d4ca729dbb5ff772c7d2216cd8ccc26ac87216d
ivicente-pt <ilidio.vicente@gmail.com>
2026-05-03 01:17:08 +0100

    Implementação de funções ui_*, bugfixes e optimmizações

--- bin/picom.sh
+++ bin/picom.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# picom.sh v1.1 2026-03-24
+# picom.sh v1.1 2026-04-07
 
-xfconf-query -c xfwm4 -p /general/use_compositing -s false --create
-[[ "${PICOM:-0}" == "1" ]] && { sleep 3; picom -b --config /opt/aevh/conf/picom/picom.conf; }
+xfconf-query -c xfwm4 -p /general/use_compositing -s false -t bool --create
+PICOM="${PICOM:-0}"
+[[ "$PICOM" -ne 1 ]] && exit 0
+killall -q picom || true
+sleep 3
+picom -b --config /opt/aevh/conf/picom/picom.conf
